5ca525604f856b070fc489cca7e1f0f936e03b86
William Sørensen <williambilly42@gmail.com>
2023-01-24 19:32:50 +0100

    Base url

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
-const BASE_URL = process.env.BASE_URL || ".";
+const BASE_URL = process.env.url || ".";
 
 const stylesHandler = isProduction
   ? MiniCssExtractPlugin.loader
